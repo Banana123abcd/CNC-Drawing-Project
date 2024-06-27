b8fcf0d391766f0a4fd36f56d195326d30e0c168
Banana123abcd <93229594+Banana123abcd@users.noreply.github.com>
2024-06-26 21:22:27 -0400

    Add files via upload
    starting ideas for how I want to code this

---
+++ CNCDrawingCode.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+***Notes to self for tommorow***
+-make cnc plane a cartesian plane. 
+-set bottom left as origin
+-select shape, parameters for shape are center point and other stuff depending on shape(radius, side length, etc)
+    - for a line other info will be needed since only center wont fork for position
+-using shape parameters "plot" points of verticies on cnc cartesian plane
+- if a point is out of plane throw an error
+- start with circle, then try triangle
+- prpbably use array for dimensions on plane and shape information
+
+*/
+
